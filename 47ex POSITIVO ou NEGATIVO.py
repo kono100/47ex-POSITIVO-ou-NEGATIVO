f296df738113c1a5af7d843a486a76ae79ba686e
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-02-19 04:03:10 -0300

    Add files via upload

---
+++ 47ex POSITIVO ou NEGATIVO.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+#47. Crie um programa que leia um valor inteiro. Em seguida, crie uma função que receba este
+#valor, mas imprima na tela informando se ele é POSITIVO ou NEGATIVO.
+
+
+
+num = int(input("Digite um Número: ")) 
+
+if num >= 0: 
+    print(f"\nNúmero {num}, é Positivo\n") 
+else: 
+    print(f"Número {num}, é Negativo\n")
